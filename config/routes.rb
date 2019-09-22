918c7b6fbc4a0c2910460fa6a043cf0413648612
femi <femilawal76@gmail.com>
2019-09-22 17:24:02 +0100

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '/make', to: 'application#make'
+
+  root 'static_pages#home'  
 end
